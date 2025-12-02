8352454066392b620c661e28a36d2959b9edec74
juniord-09 <christdofonsou@gmail.com>
2025-12-02 13:30:11 +0100

    feat: add Radix UI progress and tabs components; update package dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Geist_Mono } from "next/font/google";
-
+// import { ToastProvider } from '@/components/providers/ToastProvider'
 import "./globals.css";
 
 const inter = Inter({
@@ -30,6 +30,7 @@ export default function RootLayout({
       
       <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
         {children}
+        {/* <ToastProvider /> */}
       </body>
     </html>
   );
